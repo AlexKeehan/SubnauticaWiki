2754bca26d58116960f4e097913f44311b24b1a1
Alex Keehan <akeehan@mail.umw.edu>
2025-03-04 09:38:56 -0500

    Moved script to its own file and altered to use jQuery instead of raw JavaScript

---
+++ JavaScript/search_script.js
@@ -0,0 +1,84 @@
+
+$(document).ready(function() {
+    // Simulate a list to search within
+    // This will ultimately query the database once it is setup
+    const biomes = [
+        {name: "Blood Kelp Caves", type: "Biome", image: "Biomes/Biome Images/blood_kelp_caves.webp"},
+        {name: "Blood Kelp Zone", type: "Biome", image: "Biomes/Biome Images/blood_kelp_zone.webp"},
+        {name: "Bone Field Caves", type: "Biome", image: "Biomes/Biome Images/bone_field_caves.webp"},
+        {name: "Bulb Zone", type: "Biome", image: "Biomes/Biome Images/bulb_zone.webp"},
+        {name: "Bulb Zone Caves", type: "Biome", image: "Biomes/Biome Images/bulb_zone_caves.webp"},
+        {name: "Crag Field", type: "Biome", image: "Biomes/Biome Images/crag_field.webp"},
+        {name: "Crash Zone", type: "Biome", image: "Biomes/Biome Images/crash_zone.webp"},
+        {name: "Crash Zone Mesas", type: "Biome", image: "Biomes/Biome Images/crash_zone_mesas.webp"},
+        {name: "Crater Edge", type: "Biome", image: "Biomes/Biome Images/crater_edge.webp"},
+        {name: "Deep Grand Reef", type: "Biome", image: "Biomes/Biome Images/deep_grand_reef.webp"},
+        {name: "Deep Sparse Reef", type: "Biome", image: "Biomes/Biome Images/deep_sparse_reef.webp"},
+        {name: "Dunes", type: "Biome", image: "Biomes/Biome Images/dunes.webp"},
+        {name: "Dunes Caves", type: "Biome", image: "Biomes/Biome Images/dunes_caves.webp"},
+        {name: "Floating Island", type: "Biome", image: "Biomes/Biome Images/floating_island.webp"},
+        {name: "Grand Reef", type: "Biome", image: "Biomes/Biome Images/grand_reef.webp"},
+        {name: "Grand Reef Caves", type: "Biome", image: "Biomes/Biome Images/grand_reef_caves.webp"},
+        {name: "Grassy Plateaus", type: "Biome", image: "Biomes/Biome Images/grassy_plateaus.webp"},
+        {name: "Grassy Plateaus Caves", type: "Biome", image: "Biomes/Biome Images/grassy_plateaus_caves.webp"},
+        {name: "Inactive Lava Zone", type: "Biome", image: "Biomes/Biome Images/inactive_lava_zone.webp"},
+        {name: "Inactive Lava Zone Corridor", type: "Biome", image: "Biomes/Biome Images/inactive_lava_zone_corridor.webp"},
+        {name: "Jellyshroom Caves", type: "Biome", image: "Biomes/Biome Images/jellyshroom_caves.webp"},
+        {name: "Kelp Forest", type: "Biome", image: "Biomes/Biome Images/kelp_forest.webp"},
+        {name: "Kelp Forest Caves", type: "Biome", image: "Biomes/Biome Images/kelp_forest_caves.webp"},
+        {name: "Lava Castle", type: "Biome", image: "Biomes/Biome Images/lava_castle.webp"},
+        {name: "Lava Geyser", type: "Biome", image: "Biomes/Biome Images/lava_geyser.webp"},
+        {name: "Lava Lakes", type: "Biome", image: "Biomes/Biome Images/lava_lakes.webp"},
+        {name: "Lost River", type: "Biome", image: "Biomes/Biome Images/lost_river.webp"},
+        {name: "Mountain Island", type: "Biome", image: "Biomes/Biome Images/mountain_island.webp"},
+        {name: "Mountains", type: "Biome", image: "Biomes/Biome Images/mountains.webp"},
+        {name: "Mountains Caves", type: "Biome", image: "Biomes/Biome Images/mountains_caves.webp"},
+        {name: "Mushroom Forest", type: "Biome", image: "Biomes/Biome Images/mushroom_forest.webp"},
+        {name: "Mushroom Forest Caves", type: "Biome", image: "Biomes/Biome Images/mushroom_forest_caves.webp"},
+        {name: "Safe Shallows", type: "Biome", image: "Biomes/Biome Images/safe_shallows.webp"},
+        {name: "Safe Shallows Caves", type: "Biome", image: "Biomes/Biome Images/safe_shallows_caves.webp"},
+        {name: "Sea Treader's Path", type: "Biome", image: "Biomes/Biome Images/sea_treaders_path.webp"},
+        {name: "Sea Treader's Tunnel Caves", type: "Biome", image: "Biomes/Biome Images/sea_treaders_tunnel_caves.webp"},
+        {name: "Sparse Reef", type: "Biome", image: "Biomes/Biome Images/sparse_reef.webp"},
+        {name: "Underwater Islands", type: "Biome", image: "Biomes/Biome Images/underwater_islands.webp"},
+        {name: "Underwater Islands Caves", type: "Biome", image: "Biomes/Biome Images/underwater_islands_caves.webp"},
+    ]
+    // Get params from url
+    const query  = new URLSearchParams(window.location.search).get('query');
+
+    // Function to search for query in given list
+    function search(query) {
+        if (!query) return [];
+
+        return biomes.filter(item => item.name.toLowerCase().includes(query.toLowerCase()))
+    }
+
+    // Function to format the Biome name to redirect to correct file
+    function formatName(name) {
+        return `Biomes/${name.toLowerCase().replace(/\s+/g, '_')}.html`;
+    }
+
+    const search_results = search(query);
+
+    const resultsCont = $('#search_results');
+
+    if (search_results.length > 0) {
+        const resultsList = search_results.map(item => {
+            const biomeUrl = formatName(item.name);
+            return `
+                <div class="item">
+                    <div class="item_pic">
+                        <img src="${item.image}" alt="${item.name} image" class="item_img"/>
+                    </div>
+                    <div class="item_info">
+                        <a class="item_name" href="${biomeUrl}"><strong>${item.name}</strong></a>
+                        <p class="item_type">${item.type}</p>
+                    </div>
+                </div>`;
+        }).join('');
+        resultsCont.html(resultsList);
+    }
+    else {
+        resultsCont.html(`<p class="no_results">No results found for: <strong>'${query}'</strong></p>`);
+    }
+})
